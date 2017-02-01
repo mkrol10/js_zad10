bc2b7c77939f54a53a9645a17d4b98060e8af05a
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-02-01 21:37:16 +0100

    Improve code

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var buttonAdd = document.getElementById('addElem'),
     listAdd = document.getElementById('Lista');
 
 buttonAdd.addEventListener('click', function(e) {
-    listAdd.innerHTML += '<li>item ' + document.getElementsByTagName('li').length;
-    '</li>';
-
+    listAdd.innerHTML += '<li>item ' + document.getElementsByTagName('li').length +
+        '</li>';
 });
