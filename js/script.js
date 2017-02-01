0edd85db1317be8f4b1b014ff18347c0600da53f
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-02-01 02:27:50 +0100

    Add tenth project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var buttonAdd = document.getElementById('addElem'),
+    listAdd = document.getElementById('Lista');
+
+buttonAdd.addEventListener('click', function(e) {
+    listAdd.innerHTML += '<li>item ' + document.getElementsByTagName('li').length;
+    '</li>';
+
+});
